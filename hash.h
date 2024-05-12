f28846a434b9ef36b1f3bd1eb90068d29486e8b6
ksshkh <ksukha.3.16@gmail.com>
2024-05-12 23:08:43 +0300

    add structure for hash table, rename data

--- hash.h
+++ hash.h
@@ -3,17 +3,30 @@
 
 #include <stdio.h>
 
+//разбить функцию файнд и креэйт
+
 struct HashTable {
-    struct HashTable *next;
-    int data;
+    struct HashTableElem** hash;
+    int HashSize;
+};
+
+struct HashTableElem {
+    struct HashTableElem *next;
+    int key; 
     struct Queue *QueueElem; 
 };
 
-struct HashTable** hash_create(int n);
+struct HashTable* hash_table_create(int n);
 
-struct Queue* find(int key, struct Queue* top, struct HashTable** hash, int n);
+struct HashTableElem** hash_create(int n);
 
-struct HashTable* node_create(int key, struct Queue* top, int cell, struct HashTable** hash);
+struct Queue* find(int key, struct Queue* top, struct HashTableElem** hash, int n);
+
+struct HashTableElem* hash_table_elem_create(int key, struct Queue* top, int cell, struct HashTableElem** hash);
+
+//hash_table_elem_insert -> create -> ann's create
+
+// возращается адрес на ячейку спика хэш таблицы 
 
 int elt(int key, int n);
 
@@ -28,6 +41,10 @@ struct Queue {
 struct Queue* queue_node_create(int key, struct Queue* top);
 
 int test1(void);
+
+int test2(void);
+
+int test3(void);
 
 //////////////////////////////////////////////////////////////////////////////////////////
 
