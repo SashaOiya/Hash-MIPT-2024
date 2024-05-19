5ac408d04ad67c9934db50e911159a93790c0bed
ksshkh <ksukha.3.16@gmail.com>
2024-05-19 17:51:07 +0300

    separate tests

---
+++ hashtest.h
@@ -0,0 +1,35 @@
+#ifndef HASHTEST
+#define HASHTEST
+
+#include "hash.h"
+
+struct Queue {
+    struct QueueElem* top;
+};
+
+struct QueueElem {
+    struct QueueElem *next;
+    int key;
+};
+
+struct QueueElem* queue_node_create(int key, struct Queue* top);
+
+int test1(void);
+
+int test2(void);
+
+int test3(void);
+
+int test4(void);
+
+int test5(void);
+
+int test6(void);
+
+int test7(void);
+
+int test8(void);
+
+int test9(void);
+
+#endif
