b02687db79e6f98a4049cda6c6dee0ebf9e097a4
ksshkh <ksukha.3.16@gmail.com>
2024-05-19 14:00:00 +0300

    add new functions, list in hash was changed, destruction, recency

--- hash.cpp
+++ hash.cpp
@@ -15,6 +15,7 @@ struct HashTable* hash_table_create(int n) {
 struct HashTableElem** hash_create(int n) {
     struct HashTableElem** hash = 0;
     hash = (struct HashTableElem**)calloc(n, sizeof(struct HashTableElem*));
+    assert(hash);
     return hash;
 }
 
@@ -34,17 +35,18 @@ struct HashTableElem* find(int key, struct HashTable* HashTable) {
     return NULL;
 }
 
-void hash_table_elem_insert(int key, struct HashTable* HashTable, struct Queue* top) {
+void hash_table_elem_insert(int key, struct HashTable* HashTable, struct Queue* Queue) {
     int cell = 0;
     struct HashTableElem* new_node = 0;
     cell = elt(key, HashTable->HashSize);
-    new_node = hash_table_elem_create(key, top, HashTable);
+    new_node = hash_table_elem_create(key, Queue, HashTable);
     assert(new_node);
     if(HashTable->hash[cell] == 0) {
         HashTable->hash[cell] = new_node;
     }
     else {
         new_node->next = HashTable->hash[cell];
+        HashTable->hash[cell]->prev = new_node;
         HashTable->hash[cell] = new_node;
     }
 }
@@ -53,28 +55,62 @@ int elt(int key, int n) {
     return ((5 * key + 78) % 56) % n;
 }
 
-struct HashTableElem* hash_table_elem_create(int key, struct Queue* top, struct HashTable* HashTable) {
+struct HashTableElem* hash_table_elem_create(int key, struct Queue* Queue, struct HashTable* HashTable) {
     int cell = 0;
     cell = elt(key, HashTable->HashSize);
     struct HashTableElem* new_node = 0;
     new_node = (struct HashTableElem*)calloc(1, sizeof(struct HashTableElem));
     new_node->key = key;
-    new_node->QueueElem = queue_node_create(key, top); //smth creates node in the queue and returns pointer on the new node
+    new_node->recency = RESIDENT;
+    new_node->QueueElem = queue_node_create(key, Queue); //smth creates node in the queue and returns pointer on the new node
     return new_node;
+}
+
+void delete_hash_table_elem(struct HashTableElem* ElemForDelete, struct HashTable* HashTable) {
+    if(ElemForDelete->prev == NULL) {
+        int cell = 0;
+        cell = elt(ElemForDelete->key, HashTable->HashSize);
+        HashTable->hash[cell] = ElemForDelete->next;
+    }
+    else {
+        ElemForDelete->prev->next = ElemForDelete->next;
+    }
+}
+
+void hash_dtor(struct HashTable* HashTable) {
+    for(int i = 0; i < HashTable->HashSize; i++) {
+        if(HashTable->hash[i] != NULL) {
+            hash_list_dtor(HashTable->hash[i]);
+        }
+    }
+    free(HashTable->hash);
+    free(HashTable);
+}
+
+void hash_list_dtor(struct HashTableElem* top) {
+    while(top->next != NULL) {
+        top = top->next;
+    }
+    top = top->prev;
+    while(top->prev != NULL) {
+        free(top->next);
+        top = top->prev;
+    }
+    free(top);
 }
 
 //for testing
 //////////////////////////////////////////////////////////////////////////////////////////
 
-struct Queue* queue_node_create(int key, struct Queue* top) {
-    struct Queue* new_node = 0;
-    new_node = (struct Queue*)calloc(1, sizeof(struct Queue));
+struct Queue* queue_node_create(int key, struct Queue* Queue) {
+    struct QueueElem* new_node = 0;
+    new_node = (struct QueueElem*)calloc(1, sizeof(struct QueueElem));
     new_node->key = key;
-    if(top != NULL) {
-        new_node->next = top;
+    if(Queue->top != NULL) {
+        new_node->next = Queue->top;
     }
-    top = new_node;
-    return top;
+    Queue->top = new_node;
+    return Queue;
 }
 
 int test1(void) {
