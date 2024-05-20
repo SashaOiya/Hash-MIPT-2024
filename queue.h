840c0cce22b468cdf4893a4a1b30af235890368c
kimmater <162303643+kimmater@users.noreply.github.com>
2024-05-20 15:55:25 +0300

    Update queue.h

--- queue.h
+++ queue.h
@@ -3,9 +3,27 @@
 #define QUEUE_H
 #include "hash.h"
 
+struct QueueElem { // Structure of queue elements
+    int key;
+    struct QueueElem* next;
+    struct QueueElem* prev;
+    int recency; // resident or non_resident
+    int state;  // hir or lir
+};
+
+struct Queue {  // Queue structure
+    struct QueueElem* top;
+    struct QueueElem* bottom;
+};
+
+enum {
+    HIR,
+    LIR,
+};
+
 struct Queue* create_queue();
 void destroy_queue(struct Queue* queue);
-struct QueueElem* queue_nod_create(int key, struct Queue* Queue);
+struct QueueElem* queue_node_create(int key, struct Queue* Queue);
 void enqueue(struct Queue* Queue, int key);
 void dequeue(struct Queue* Queue);
 void lift_queue_element(struct Queue* Queue, struct QueueElem* element);
